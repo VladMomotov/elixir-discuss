c8cf1e1e1aeeeffabe239b45f55488b8cc7dbc53
Vlad Momotov <vladislav.momatau@itechart-group.com>
2020-02-27 16:25:55 +0300

    #101

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -2,7 +2,15 @@ defmodule Discuss.AuthController do
     use Discuss.Web, :controller
     plug Ueberauth
 
-    def callback(conn, params) do
-        
+    alias Discuss.User
+
+    def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+        user_params = %{
+            token: auth.credentials.token,
+            email: auth.info.email,
+            provider: auth.provider
+        }
+
+        changeset = User.changeset(%User{}, user_params)
     end
 end
