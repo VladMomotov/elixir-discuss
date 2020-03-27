321c899f70c61b4151947a33122a254adcd5e1bc
Vladislav Momatau <vlad.momotov@gmail.com>
2020-03-27 15:07:56 +0300

    #125

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,9 +1,13 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
-  def join() do
+  def join(topic, _auth_msg, socket) do
+    IO.puts("+++++++++")
+    IO.puts(topic)
+
+    {:ok, %{hey: "there"}, socket}
   end
 
-  def handle_in() do
+  def handle_in(_event, _msg, _arg2) do
   end
 end
