9e1ef179db9b885970276241f75dd398e857db6b
Vladislav Momatau <vlad.momotov@gmail.com>
2020-04-03 18:04:59 +0300

    make prod.exs equal to phoenix 1.4.16

--- config/prod.exs
+++ config/prod.exs
@@ -18,21 +18,6 @@ config :discuss, DiscussWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :discuss, DiscussWeb.Endpoint,
-  load_from_system_env: true,
-  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
-  server: true, # Without this line, your app will not start the web server!
-  secret_key_base: "${SECRET_KEY_BASE}",
-  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json"
-
-config :discuss, Discuss.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: "${DATABASE_URL}",
-  database: "",
-  ssl: true,
-  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
-
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
@@ -72,4 +57,4 @@ config :discuss, Discuss.Repo,
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-# import_config "prod.secret.exs"
+import_config "prod.secret.exs"
--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -20,4 +20,8 @@ secret_key_base =
     """
 
 config :discuss, DiscussWeb.Endpoint,
+  http: [
+    port: String.to_integer(System.get_env("PORT") || "4000"),
+    transport_options: [socket_opts: [:inet6]]
+  ],
   secret_key_base: secret_key_base
