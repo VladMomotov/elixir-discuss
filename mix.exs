0d86e9102046c21197020c92e32c5be59c5b95d9
Vladislav Momatau <vlad.momotov@gmail.com>
2020-04-03 14:44:56 +0300

    use elixir 1.9

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Discuss.Mixfile do
     [
       app: :discuss,
       version: "0.0.1",
-      elixir: "~> 1.5",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       build_embedded: Mix.env() == :prod,
