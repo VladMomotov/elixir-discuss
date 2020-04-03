e14e52b8358569a4b3ff60e058d684dd57d48757
Vladislav Momatau <vlad.momotov@gmail.com>
2020-04-03 19:46:05 +0300

    config ueberauth in prod

--- config/releases.exs
+++ config/releases.exs
@@ -4,3 +4,7 @@ config :discuss, DiscussWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: System.get_env("GITHUB_OAUTH_CLIENT_ID"),
+  client_secret: System.get_env("GITHUB_OAUTH_CLIENT_SECRET")
